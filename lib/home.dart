214c9cbeec2dc01117d45633cb7c3e013b260299
Doplin <soeunsense7@gmail.com>
2023-01-27 01:06:24 +0900

    feat[#4]: post view ruff하게 제작
    - main.dart: view 관련 코드 home.dart로 분리, theme 적용
    - add home.dart: add home page 확장성 고려
    - post_view.dart: post view ruff하게 제작
    
    issue
    - list view 에러 해결

---
+++ lib/home.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
+
+import 'post/view/post_view.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    final pageList = [
+      const PostView(),
+    ];
+
+    // TODO: navigation bar 있다면 추가 return GetBuilder<NavigationController>
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
--- lib/post/view/post_view.dart
+++ lib/post/view/post_view.dart
@@ -1,41 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_norithon_team0/post/controller/post_controller.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
-import 'package:get/get.dart';
 
 class PostView extends StatefulWidget {
   const PostView({super.key});
 
   @override
-  State<PostView> createState() => _PostViewState();
+  State<PostView> createState() => PostViewState();
 }
 
-class _PostViewState extends State<PostView> {
-  final controller = Get.put(PostController());
-
+class PostViewState extends State<PostView> {
   @override
   Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    // final textTheme = Theme.of(context).textTheme;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text("post"),
-        actions: [
-          IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.person,
-              ))
-        ],
-      ),
-      body: Center(
-        child: GetBuilder<PostController>(
-          builder: (context) {
-            return Container(
-              padding: EdgeInsets.all(10),
-              child: Text("post"),
+        appBar: AppBar(
+            shadowColor: colorScheme.shadow,
+            elevation: 1.0,
+            centerTitle: true,
+            title: const Text('AppName'
+                // TODO: add font style
+                )),
+        body: Center(
+          child: GetBuilder<PostController>(builder: (_postController) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  height: 200,
+                  child: ListView(children: const <Widget>[
+                    Card(
+                      child: ListTile(
+                        leading: FlutterLogo(),
+                        title: Text('text1'),
+                      ),
+                    ),
+                    Card(
+                      child: ListTile(
+                        leading: FlutterLogo(),
+                        title: Text('text1'),
+                      ),
+                    )
+                  ]),
+                )
+              ],
             );
-          },
-        ),
-      ),
-    );
+          }),
+        ));
   }
 }
