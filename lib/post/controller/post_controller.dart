eba7fcabf80d26cdd220e26d032977c52255992b
chanhk-im <cfasd1875@gmail.com>
2023-01-27 15:12:25 +0900

    feat[#1] : post controller update, delete
    - post controller의 update, delete 기능 추가

--- lib/post/controller/post_controller.dart
+++ lib/post/controller/post_controller.dart
@@ -1,29 +1,36 @@
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
-import 'package:firebase_core/firebase_core.dart';
 
 import 'package:flutter_norithon_team0/post/model/post.dart';
 import 'package:flutter_norithon_team0/post/application/post_application.dart';
 
 class PostController extends GetxController {
   List<Post> postList = [];
-  PostApplication _postApplication = PostApplication();
+  final PostApplication _postApplication = PostApplication();
 
   Future<void> createPost(Post post) async {
     print("create");
     await _postApplication.createPost(post);
-    await fetchPost();
+    await fetchPosts();
   }
 
-  Future<void> fetchPost() async {
-    await _postApplication.fetchPosts(postList);
+  Future<void> fetchPosts() async {
+    await _postApplication.getPosts(postList);
     update();
+  }
+
+  Future<void> updatePost(Post post) async {
+    await _postApplication.updatePost(post);
+    await fetchPosts();
+  }
+
+  Future<void> deletePost(Post post) async {
+    await _postApplication.deletePost(post.id!);
+    await fetchPosts();
   }
 
   @override
   void onInit() {
     super.onInit();
-    fetchPost();
+    fetchPosts();
   }
 }
