0e03b39a669d2b8b3a1e18f4952d6bf6bd200bf7
Doplin <soeunsense7@gmail.com>
2023-01-27 22:17:20 +0900

    feat[#19]: create add_post_view.dart

---
+++ lib/post/view/add_post_view.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_norithon_team0/post/controller/post_controller.dart';
+import 'package:flutter_norithon_team0/widget/grid_view.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
+
+class AddPostView extends StatefulWidget {
+  const AddPostView({super.key});
+
+  @override
+  State<AddPostView> createState() => AddPostViewState();
+}
+
+class AddPostViewState extends State<AddPostView> {
+  List<String> postList = ['text1', 'text2'];
+
+  Widget postListTile(BuildContext context) {
+    // return GetBuilder<PostController>(builder: (_postController) {
+    // return ListView.builder(
+    return Expanded(
+      child: ListView.builder(
+          padding: const EdgeInsets.all(20),
+          // itemCount: _postController.postList.length * 2,
+          itemCount: 2,
+          itemBuilder: (context, i) {
+            return Text(
+              postList[i],
+              // )
+              // return Card(
+              //   child: ListTile(
+              //     // leading: FlutterLogo(),
+              //     title: Text(postList[i]),
+              //   ),
+            );
+          }),
+    );
+    // });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    // final textTheme = Theme.of(context).textTheme;
+
+    return Scaffold(
+      appBar: AppBar(
+          shadowColor: colorScheme.shadow,
+          elevation: 1.0,
+          centerTitle: true,
+          title: const Text('AppName'
+              // TODO: add font style
+              )),
+      // body: RefreshIndicator(
+      //     onRefresh: () {
+      //       Future temp = "hi" as Future;
+      //       return temp;
+      //     },
+      // body: Container(
+      //   margin: const EdgeInsets.all(10.0),
+      //   width: 48.0,
+      //   height: 48.0,
+      body: postListTile(context),
+      // )
+      // )
+    );
+  }
+}
