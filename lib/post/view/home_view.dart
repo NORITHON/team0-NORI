2efa9341817ee25f0fbc59b9499ef8eec2d4bd98
tjdgml97 <parksunghee9@naver.com>
2023-01-28 01:16:48 +0900

    feat[#30] : 홈화면에 프로필 내용 추가
    프로필관련 아이콘, 버튼 추가

--- lib/post/view/home_view.dart
+++ lib/post/view/home_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -11,12 +10,104 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: TextButton(
-        child: Text("home->addPost"),
-        onPressed: () {
-          // Get.to(() => const HomeView());
-        },
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+              padding: EdgeInsets.fromLTRB(30, 50, 30, 0),
+              color: Colors.white,
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          Navigator.pop(context);
+                          print("hi");
+                        },
+                        child: // ),
+                            Image.asset(
+                                width: 30,
+                                height: 30,
+                                'assets/button/button-heart.png'),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      InkWell(
+                        onTap: () {
+                          Navigator.pop(context);
+                          print("hi");
+                        },
+                        child: // ),
+                            Image.asset(
+                                width: 30,
+                                height: 30,
+                                'assets/button/button-childmode.png'),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                      Image.asset(
+                          width: 70,
+                          height: 70,
+                          'assets/image/image-profile.png'),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Container(
+                        child: Column(
+                          children: [
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Text(
+                                  "성희",
+                                  style: TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                SizedBox(width: 10),
+                                Text(
+                                  "10세 . 남",
+                                  style: TextStyle(fontSize: 12),
+                                ),
+                                SizedBox(width: 100),
+                              ],
+                            ),
+                            SizedBox(height: 10),
+                            Image.asset(
+                                width: 256,
+                                height: 24,
+                                'assets/image/image-bar.png'),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                ],
+              )),
+          SizedBox(
+            height: 20,
+          ),
+          Divider(
+            //color: Colors.grey,
+            height: 30,
+            thickness: 9,
+          ),
+        ],
       ),
     );
   }
